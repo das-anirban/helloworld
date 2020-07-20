57df8f2cc7c411aad844e8038662e065372e9056
Anirban Das <adas78@its.jnj.com>
2020-07-21 03:05:01 +0530

    changing the comments

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 from random import randint
 import sys
 
-'''
-Hey I am new to Github. 
-This is my first repo
-'''
+
+# Hey I am new to Github. 
+# This is my first repo
+
 text = input("Enter your name to say hello to ")
 print(f"Hello {text}, how are you doing.")
 
 while True:
 	choice = input("Do you want to play the guessing game? y/n")
-	if(choice == 'Y' or choice == 'y'):
+    if(choice == 'Y' or choice == 'y'):
 		start = 1
 		end = 10
 
